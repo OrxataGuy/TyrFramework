f496748f76b0f5286c7aa57fb982f6ac382b1e0e
Manel Andreu <mandreu@avantio.com>
2026-03-01 20:42:59 +0100

    changes

--- tests/commands.test.ts
+++ tests/commands.test.ts
@@ -8,7 +8,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const frameworkRoot = path.resolve(__dirname, '..');
-console.log(__dirname, __filename, frameworkRoot)
 
 interface TyrConfig {
     commands: Record<string, string>;
